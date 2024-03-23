2eb104747143604361c57b51815f443929aec454
Micah Longmire <mlmicahlongmire@gmail.com>
2024-03-23 16:30:14 -0600

    potential fix

--- pxmx_cli/commands/vm-kill.py
+++ pxmx_cli/commands/vm-kill.py
@@ -1,15 +1,17 @@
 import click
 import paramiko
+import getpass
 
+# Define the Click command
 @click.command()
 @click.argument('vm_ids', nargs=-1, required=True)
 @click.pass_context
 def vm_kill(ctx, vm_ids):
-    """Kill VM(s) by ID."""
+    """Kill VM(s) by ID and clean up logs."""
     config = ctx.obj
     proxmox_ip = config['proxmox_ip']
-    username = config['username']
-    password = config['password']  # Assuming password is safely handled
+    username = 'root'
+    password = getpass.getpass(f"Enter the password for {username}@{proxmox_ip}: ")
 
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -19,28 +21,27 @@ def vm_kill(ctx, vm_ids):
 
         for vm_id in vm_ids:
             click.echo(f"Processing VM {vm_id}...")
-
-            # Check if VM exists
-            stdin, stdout, stderr = ssh.exec_command(f"qm list | grep -qw '{vm_id}'")
-            if stderr.read().decode('utf-8'):
-                click.echo(f"VM {vm_id} does not exist.")
-                continue  # Skip to the next VM ID if the current one does not exist
-
-            # Proceed with VM killing steps
             commands = [
-                f"rm -f /var/lock/qemu-server/lock-{vm_id}.conf",
-                f"qm stop {vm_id}",
-                f"sed -i '/^lock:/d' /etc/pve/nodes/proxmox/qemu-server/{vm_id}.conf",
-                f"qm destroy {vm_id}"
+                f"echo 'Attempting to stop VM {vm_id}...' && qm stop {vm_id} && sleep 5",
+                f"echo 'Removing lock file for VM {vm_id}...' && rm -f /var/lock/qemu-server/lock-{vm_id}.conf",
+                f"echo 'Removing lock status from VM {vm_id} configuration...' && sed -i '/^lock:/d' /etc/pve/nodes/proxmox/qemu-server/{vm_id}.conf",
+                f"echo 'Destroying VM {vm_id}...' && qm destroy {vm_id} && sleep 2",
             ]
 
             for command in commands:
-                stdin, stdout, stderr = ssh.exec_command(command)
+                _, stdout, stderr = ssh.exec_command(command)
+                click.echo(stdout.read().decode('utf-8'))
                 error = stderr.read().decode('utf-8')
                 if error:
+                    # Ask if the user wants to clean logs upon error
+                    cleanup_logs = click.confirm(f"Error executing command '{command}': {error}. Do you want to clean up logs for this VM?")
+                    if cleanup_logs:
+                        ssh.exec_command(f"cp /var/log/pve/tasks/active /var/log/pve/tasks/active.backup && sed -i '/:qm[^:]*:{vm_id}:/d' /var/log/pve/tasks/active && echo 'Log cleanup for VM {vm_id} completed.'")
+                    else:
+                        click.echo(f"Log cleanup for VM {vm_id} was not performed.")
                     raise Exception(f"Error executing command '{command}': {error}")
 
-            click.echo(f"VM {vm_id} has been destroyed.")
+            click.echo(f"VM {vm_id} has been processed.")
 
     except paramiko.AuthenticationException:
         click.echo("Authentication failed, please verify your credentials.")
